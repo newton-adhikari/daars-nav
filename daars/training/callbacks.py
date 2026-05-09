8b0cfa9643d8160e2834b198129b35bd795af236
Newton Adhikari <newtonadk@gmail.com>
2026-05-09 10:49:32 +0545

    defined constants for callback

--- daars/training/callbacks.py
+++ daars/training/callbacks.py
@@ -13,6 +13,25 @@ class NavCallback(BaseCallback):
     """
 
     def __init__(
-        self
+        self,
+        reward_type: str = "daars",
+        total_timesteps: int = 1_000_000,
+        log_interval: int = 10_000,
+        verbose: int = 0,
     ):
-        super().__init__()
+        self.reward_type     = reward_type
+        self.total_timesteps = total_timesteps
+        self.log_interval    = log_interval
+
+        self.rewards         : list[float] = []
+        self.lengths         : list[int]   = []
+        self.successes       : list[bool]  = []
+        self.collisions      : list[bool]  = []
+        self.min_clearances  : list[float] = []
+        self.cumulative_costs: list[float] = []
+
+        self._ep_reward  = 0.0
+        self._ep_len     = 0
+        self._start_time = None
+        self._last_log   = 0
+
